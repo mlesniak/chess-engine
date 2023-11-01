24519c72d6b13947acd0bdc980496a8a1f3e24ca
Michael Lesniak <mail@mlesniak.com>
2023-11-01 13:21:02 +0100

    Restart engine algorithm

--- Engine.cs
+++ Engine.cs
@@ -1,56 +1,32 @@
-// Ignore caching, pruning, etc. for the time being.
-// Come up with the simplest, even if inefficient,
-// algorithm possible.
 public class Engine
 {
-    private static readonly int maxDepth = 3;
+    private static readonly int maxDepth = 0;
 
-    // For every move, find the one with the highest score for 
-    // the current side. Then choose this, switch sides, find 
-    // the best response and switch again until our depth is
-    // exhausted.
+    // Score: positive: better for white, negative: better for black.
     public (Move, int) NextBestMove(Game game, Color currentColor, int depth = 0)
     {
         List<Move> allMoves = game.ValidMoves();
 
-        // Enrich this we the respective score, such that 
-        // we have tuples of moves and their score effect.
-
-        // TODO(mlesniak) enrich this...
-
         if (depth == maxDepth)
         {
-            // Find best move and return it.
-            allMoves.Sort((m1, m2) =>
-            {
-                var g1 = game.Move(m1);
-                var g2 = game.Move(m2);
+            var bestMove = allMoves
+                .Select(move =>
+                {
+                    var score = game.Score();
+                    return (move, score);
+                })
+                .OrderBy<(Move, int), int>(t => t.Item2)
+                .ToList();
 
-                return g2.Score() - g1.Score();
-            });
-            return (allMoves[0];
+            if (currentColor == Color.White)
+            {
+                // Choose biggest score.
+                return bestMove.Last();
+            }
+            
+            return bestMove.First();
         }
 
-        // We have not reached maximal depth for searching.
-        // Branch for every single move. 
-
-
-
-        // allValidMoves.Sort((m1, m2) =>
-        // {
-        //     var g1 = game.Move(m1);
-        //     var g2 = game.Move(m2);
-        //
-        //     return g2.Score() - g1.Score();
-        // });
-        //
-        // var bestMove = allValidMoves[0];
-        // if (depth > maxDepth)
-        // {
-        //     return bestMove;
-        // }
-
-        // Apply move to game, switch sides, and find best move for
-        // the other side based on this one.
+        throw new NotImplementedException();
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,16 +1,4 @@
-﻿// Game root = new Game();
-
-// Engine engine = new Engine();
-// var nextMove = engine.NextBestMove(root, Piece.PieceColor.White);
-// Console.WriteLine("nextMove = {0}", nextMove);
-//
-// var n = root.Move(nextMove);
-// Console.WriteLine(n);
-//
-
-using static Color;
-
-Game root  = Loader.Load("game.txt");
+﻿Game root  = Loader.Load("game.txt");
 Console.WriteLine("Original game");
 Console.WriteLine(root.ToString());
 Console.WriteLine("------------------");
@@ -18,8 +6,13 @@ Console.WriteLine("------------------");
 
 // TODO(mlesniak) implement basic min-maxing with at least a depth of 3 showing that white always captures the king
 //                we might need to add some immovable objects for testing.
-root.Turn = Black;
+root.Turn = Color.White;
 foreach (var move in root.ValidMoves())
 {
-    Console.WriteLine($"{move}");
+    Console.Write($"{move}\t");
 }
+Console.WriteLine();
+
+Engine engine = new Engine();
+var bestMove = engine.NextBestMove(root, Color.White);
+Console.WriteLine($"{bestMove}");
